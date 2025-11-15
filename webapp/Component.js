c8f806d0aeea6ef174fd5c458c138785cc0169cf
BHARGAVA SRI SAI KRISHNA BUDDANA <bbsskrishna@gmail.com>
2025-11-15 10:15:14 +0000

    basic route added

--- webapp/Component.js
+++ webapp/Component.js
@@ -21,6 +21,8 @@ sap.ui.define([
                 var oDeviceModel = new JSONModel(Device);
                 oDeviceModel.setDefaultBindingMode("OneWay");
                 this.setModel(oDeviceModel, "device");
+                // enable routing
+                this.getRouter().initialize();
             },
 
             getContentDensityClass: function () {
